f50cbfba7095b75123c50785029319096cadbe92
Marek Czuma <marek.czuma@gmail.com>
2015-03-10 12:52:49 +0100

    devise gem added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,18 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  # Default URL options for mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  #domain:               'example.com',
+  user_name:            'mikro.tutoriale',
+  password:             'HWilliams',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
